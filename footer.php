5ee00391a6028f36d80b6fcfd75ba9a3b67e13f4
chum9625 <17173386+chum9625@users.noreply.github.com>
2021-08-30 21:15:57 +0900

    フッター固定メニュー記述箇所移動、h4イメージ変更

--- footer.php
+++ footer.php
@@ -1,24 +1,32 @@
 	<div class="pagetop js-pagetop"><i class="fas fa-angle-up"></i>PAGE TOP</div>
 
 	<footer class="footer">
-	  <div class="container">
-		<div class="footer_inner">
-		  <nav class="gnavMainFooter">
-				<?php
-				$args = array(
+        <div class="container">
+            <div class="footer_inner">
+                <nav class="gnavMainFooter">
+                    <?php
+                    $args = array(
 					'theme_location' => 'footer-menu',
 					'container'  => false,
-				);
-				wp_nav_menu( $args );
-				?>
-		  </nav>
-		  <div class="footer_copyright">
-			<small>&copy; Noble Venus</small>
-		  </div>
-		</div>
-	  </div>
-	</footer>
+                    );
+                    wp_nav_menu( $args );
+                    ?>
+                </nav>
+                <div class="footer_copyright">
+                    <small>&copy; Noble Venus</small>
+                </div>
 
+            </div>
+        </div>
+    </footer>
+
+    <div class="for-sp sp-fixed-menu">
+        <ul>
+            <li><a href="<?php echo home_url() ?>/kind/treatment/">施術</a></li>
+            <li><a href="<?php echo home_url() ?>/contact"><i class="far fa-envelope fa-lg"></i></a></li>
+            <li><a href="<?php echo home_url() ?>/kind/lesson/">講座</a></li>
+        </ul>
+    </div>
 	<?php
 	if ( is_home() ) {
 		wp_enqueue_style( 'slick-carousel', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css' );
@@ -26,15 +34,6 @@
 		wp_enqueue_script( 'tomo-venus-home', get_template_directory_uri() . '/assets/js/home.js' );
 	}
 	?>
-
-	<?php wp_footer(); ?>
-    <div id="sp-fixed-menu" class="for-sp">
-  <ul>
-      <li><a href="<?php echo home_url() ?>/kind/treatment/">施術</a></li>
-      <li><a href="<?php echo home_url() ?>/contact"><i class="far fa-envelope fa-lg"></i></a></li>
-    <li><a href="<?php echo home_url() ?>/kind/lesson/">講座</a></li>
-</ul>
-</div>
-	</body>
-
-	</html>
+    <?php wp_footer(); ?>
+</body>
+</html>
